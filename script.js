41092d73336da0b30cf455f987c98c789f51ace2
Sam Quinn <lazersmoke@gmail.com>
2020-09-24 15:59:20 -0400

    Add exploration feature

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 // Each bundle exports a global object with the name of the bundle.
+const ZDIMS = 256
+
+const modelUrl = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small'
+//const modelUrl = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_small_q2'
+//const modelUrl = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/groovae_4bar'
+const model = new music_vae.MusicVAE(modelUrl)
+const modelDecode = xs => {
+  //const temperature = 0.5
+  const temperature = 0.0001
+  const stepsPerQuarter = 4
+  const qpm = 120
+  return model.decode(xs, temperature, undefined, stepsPerQuarter, qpm)
+}
+
+var currentBaseZ = undefined;
+var isPlaying = false
 
 window.onload = () => {
-  const noteSeq = {notes: [{pitch: 60, startTime: 0.0, endTime: 0.5},{pitch: 60, startTime: 0.5, endTime: 1.0},{pitch: 67, startTime: 1.0, endTime: 1.5}], totalTime: 1.5}
+  const controls = document.getElementById("controlArea")
+  controls.style.width = "200px"
+  const spreadSlider = document.createElement("input")
+  const spreadDisplay = document.createElement("span")
+  const spreadDisplayText = document.createTextNode("0")
+  spreadDisplay.appendChild(spreadDisplayText)
+  spreadSlider.type = "range"
+  spreadSlider.min = "0"
+  spreadSlider.max = "100"
+  spreadSlider.value = "0"
+  spreadSlider.disabled = true
+  spreadSlider.id = "spreadSlider"
+  controls.appendChild(document.createTextNode("Spread: "))
+  controls.appendChild(spreadSlider)
+  controls.appendChild(spreadDisplay)
+  controls.appendChild(document.createElement("br"))
+  spreadSlider.addEventListener("input",() => spreadDisplayText.textContent = spreadSlider.value/100)
 
+  const regenButton = document.createElement("input")
+  regenButton.value = "Regenerate"
+  regenButton.type = "button"
+  regenButton.disabled = true
+  controls.appendChild(regenButton)
 
 /*
  * config.json
@@ -18,45 +55,127 @@ window.onload = () => {
   }
 }
 */
-  //const modelUrl = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small'
-  //const modelUrl = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_small_q2'
-  const modelUrl = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/groovae_4bar'
-  const model = new music_vae.MusicVAE(modelUrl)
   model.initialize().then(() => {
-    const numSamples = 3
-    const randZs = tf.tidy(() => tf.randomNormal([numSamples, model.decoder.zDims]))
-    document.getElementById("summaryBox").innerHTML = randZs.toString()
-    const temperature = 0.5
-    const stepsPerQuarter = 4
-    const qpm = 120
-    const outputSequences = model.decode(randZs, temperature, undefined, stepsPerQuarter, qpm)
-    randZs.dispose()
-    outputSequences.then((samples) => {
-      samples.forEach((notes,i) => {
-        //const notes = samples[0]
-        //notes = core.sequences.unquantizeSequence(samples[0])
+    currentBaseZ = samplePrior()
+    regenButton.addEventListener("click",() => regenExamples(currentBaseZ.clone()))
 
-        const canv = document.createElement("canvas")
-        canv.classList.add("pianoroll")
-        document.getElementById("canvasHolder").appendChild(canv)
-
-        const viz = new core.PianoRollCanvasVisualizer(notes, canv);
-        const vizPlayer = new core.Player(false, {
-          run: (note) => viz.redraw(note),
-          stop: () => {}
-        });
-        let isPlaying = false
-        canv.addEventListener("click",() => {
-          if(isPlaying){
-            vizPlayer.stop()
-          }
-          if(!isPlaying){
-            vizPlayer.start(notes)
-          }
-          isPlaying = !isPlaying
-        })
-        //vizPlayer.start(notes)
-      })
-    });
+    modelDecode(applyPerturbations(currentBaseZ.clone(),makeRandomPerturbations(50,0.3))).then(() => {
+      regenButton.disabled = false
+      spreadSlider.disabled = false
+    })
   })
 }
+
+function regenExamples(base){
+  const numPerturb = 8
+  const spreadAmount = document.getElementById("spreadSlider").value/100
+  const perturbs = makeRandomPerturbations(numPerturb,spreadAmount)
+  const perturbed = applyPerturbations(base,perturbs)
+
+  document.getElementById("summaryBox").innerHTML = perturbs.toString() + "\n\n" + getSumStats(perturbs).toString()
+  tfvis.render.heatmap(document.getElementById("tfvisThing"),{values: getSumStats(perturbs)})
+  const baseSequence = modelDecode(base.expandDims())
+  const outputSequences = modelDecode(perturbed)
+
+  const canvHolder = document.getElementById("canvasHolder")
+  while (canvHolder.firstChild) {
+    canvHolder.removeChild(canvHolder.lastChild);
+  }
+
+  baseSequence.then(samples => {
+    // Only one base sequence
+    displaySequence(samples[0],base,"Base")
+  })
+  outputSequences.then(samples => {
+    samples.forEach((notes,i) => {
+      displaySequence(notes, perturbed.slice(i,1).squeeze(), "Perturbation #" + (i + 1))
+    })
+  });
+}
+
+function displaySequence(seq,zCode,name = "Unamed Note Sequence"){
+  seq = core.sequences.unquantizeSequence(seq)
+  const canvLabel = document.createElement("span")
+  const canvLabelText = document.createTextNode(name)
+  canvLabel.appendChild(canvLabelText)
+  const canv = document.createElement("canvas")
+  canv.classList.add("pianoroll")
+
+
+  const changeButton = document.createElement("input")
+  changeButton.type = "button"
+  changeButton.value = "Use " + name
+  changeButton.addEventListener("click",() => {
+    currentBaseZ = zCode.clone()
+    regenExamples(currentBaseZ)
+  })
+  document.getElementById("canvasHolder").appendChild(canvLabel)
+  document.getElementById("canvasHolder").appendChild(changeButton)
+  document.getElementById("canvasHolder").appendChild(canv)
+
+  const viz = new core.PianoRollCanvasVisualizer(seq, canv, {noteHeight: 2, pixelsPerTimeStep: 20});
+  const vizPlayer = new core.Player(false, {
+    run: (note) => viz.redraw(note),
+    stop: () => {isPlaying = false}
+  });
+  canv.addEventListener("click",() => {
+    if(isPlaying){
+      vizPlayer.stop()
+    }
+    if(!isPlaying){
+      vizPlayer.start(seq)
+    }
+    isPlaying = !isPlaying
+  })
+}
+
+function getRandZs(n){
+  return tf.tidy(() => tf.randomNormal([n, ZDIMS]))
+}
+
+function samplePrior(){
+  return getRandZs(1).squeeze()
+}
+
+function makeRandomPerturbations(n,spread = 0.1){
+  return getRandZs(n).mul(spread)
+}
+
+function applyPerturbations(base,deltas){
+  return base.expandDims().tile([deltas.shape[0],1]).add(deltas)
+}
+
+function makeRandomExamples(n,biasSize = 0,randomness = 0.1){
+  const origs = getRandZs(n)
+  const bias = getRandZs(1).squeeze().mul(biasSize)
+  const perturbations = getRandZs(n).mul(randomness)
+  return tf.stack([origs,origs.add(perturbations).add(bias)],1)
+}
+
+// examplePairs shape is [numExamples,2,ZDIMS]
+function monkeySee(examplePairs){
+  return tf.tidy(() => {
+    const [orig, mod] = tf.split(examplePairs,2,1)
+    const deltas = mod.sub(orig).squeeze()
+    console.log("Deltas: ",deltas.toString())
+    const sumStats = deltas.matMul(deltas.transpose())
+    console.log("Sum Stats: ",sumStats.toString())
+    const diag = tf.linalg.bandPart(sumStats,0,0).sum(1)
+    const trace = diag.sum(0)
+    diag.print()
+    trace.print()
+    const reducedStats = sumStats.div(trace)
+    return reducedStats
+  })
+}
+
+function getSumStats(deltas){
+  return tf.tidy(() => {
+    const sumStats = deltas.matMul(deltas.transpose())
+    const diag = tf.linalg.bandPart(sumStats,0,0).sum(1)
+    const trace = diag.sum(0)
+    const reducedStats = sumStats.div(trace)
+    return reducedStats
+  })
+}
+
